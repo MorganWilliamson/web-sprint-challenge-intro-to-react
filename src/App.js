268738c11320d4651ec2c79effba944068c71235
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-09 10:52:45 -0500

    Setting up get request.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,31 @@ import axios from 'axios'
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characterData, setCharacterData] = useState('');
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+      .then(res => {
+        setCharacterData(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+}, []) 
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+return (
+  <div className="App">
+    <h1 className="Header">Characters</h1>
+   
+  </div>
+);
 }
 
 export default App;
 
 // Star Wars API: https://swapi.dev/
+// API link to character list: https://swapi.dev/api/people
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,4 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 
+
 // Write your Character component here
+
+function Character(props){
+    console.log(props)
+    
+    return props;
+}
+
+export default Character;
